fe3da8f6c9bf12778f8148bc032015a1dbfab11b
Mohd Samid 78 <mohdsamid78@gmail.com>
2025-11-17 23:59:11 +0530

    Refactor lec9.java to remove unnecessary comment

--- src/lec9.java
+++ src/lec9.java
@@ -1,28 +1,19 @@
 public class lec9 {
-// Parent class
 class Employee {
     void work() {
         System.out.println("Employee is working.");
     }
 }
-
-// Child class inheriting Employee
 class Manager extends Employee {
     void attendMeeting() {
         System.out.println("Manager is attending a meeting.");
     }
 }
-
-// Main class to run the program
 public class Main {
     public static void main(String[] args) {
-
-        // Creating object of Manager
         Manager m = new Manager();
-
-        // Calling methods
-        m.work();           // inherited from Employee
-        m.attendMeeting();  // Manager's own method
+        m.work();
+        m.attendMeeting(); 
     }
 }
 }
